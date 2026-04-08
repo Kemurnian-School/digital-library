a0af6b39d1ef13ace209d36077def2ac3e4853f2
MystiaFin <cytrsx01@gmail.com>
2026-04-08 11:23:39 +0700

    chore: include pdfviewer when running build

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 "resources/css/app.css",
                 "resources/js/app.js",
                 "resources/images/logo.png",
+                "resources/js/pdfviewer.js"
             ],
             refresh: true,
         }),
